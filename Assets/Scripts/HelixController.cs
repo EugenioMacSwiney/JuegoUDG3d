42dfc376558f367a531595bf1ed6a853402edb7b
EugenioMacSwiney <eugenio.mac@udgvirtual.udg.mx>
2025-04-07 13:37:26 -0600

    4.3. Niveles de juego

--- Assets/Scripts/HelixController.cs
+++ Assets/Scripts/HelixController.cs
@@ -1,18 +1,30 @@
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class NewMonoBehaviourScript : MonoBehaviour
+
 {
 
-private Vector2 lastTapPosition;
-private Vector3 startPosition;
-    void Start()
-    {
-        startPosition = transform.localEulerAngles;
 
+private Vector2 lastTapPosition;
+private Vector3 startRotation;
+public Transform topTransform;
+public Transform goalTransform;
+public GameObject helixLevelPrefab;
+public List<Stage> allStages = new List<Stage>();
+public float helixDistance;
+private List<GameObject> spawnedLevels = new List<GameObject>();
+
+    private void Awake()
+    {
+        startRotation = transform.localEulerAngles;
+        helixDistance = topTransform.localPosition.y - (goalTransform.localPosition.y + 0.1f);
+        LoadStage(0);
     }
 
- 
-    void Update()
+     void Update()
     {
         if (Input.GetMouseButton(0))
         {
@@ -30,4 +42,36 @@ private Vector3 startPosition;
             lastTapPosition = Vector2.zero;
         }
     }
-}
+
+    public void LoadStage(int stageNumbrer)
+    {
+        Stage stage = allStages[Mathf.Clamp(stageNumbrer, 0, allStages.Count - 1)];
+        if(stage == null)
+        {
+           Debug.Log("sin niveles");
+           return;
+        }
+        Camera.main.backgroundColor = allStages[stageNumbrer].stageBackgroundColor;
+        FindFirstObjectByType<BallController>().GetComponent<Renderer>().material.color = allStages[stageNumbrer].stageBallColor;
+        transform.localEulerAngles = startRotation;
+        foreach (GameObject go in spawnedLevels)
+        {
+            Destroy(go);
+        }
+        float levelDistance = helixDistance / stage.levels.Count;
+        float spawnPosY = topTransform.localPosition.y;
+      
+           
+        for (int i = 0; i < stage.levels.Count; i++)
+              
+        {
+            spawnPosY -= levelDistance; 
+            GameObject level = Instantiate(helixLevelPrefab, transform);
+            level.transform.localPosition = new Vector3(0, spawnPosY, 0);
+            spawnedLevels.Add(level);
+        }
+
+
+        }  
+    }  
+       
